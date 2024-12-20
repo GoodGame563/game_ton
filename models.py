562587901859c0a7a600dd56cc4f50df39718ccb
GoodGame563 <124364912+GoodGame563@users.noreply.github.com>
2024-12-20 18:48:24 +0300

    Update models.py

--- models.py
+++ models.py
@@ -1,33 +1,70 @@
-from pydantic import BaseModel
+from typing import List, Optional
+from pydantic import BaseModel, Field
+from datetime import datetime
 
-class OurSnake(BaseModel):
-    id:int
-    direction: str
-    oldDirection: str
-    geometry: list
-    deathCount: int
+
+class Direction3D(BaseModel):
+    x: int
+    y: int
+    z: int
+
+class Point3D(BaseModel):
+    x: int
+    y: int
+    z: int
+
+class SnakeRequestItem(BaseModel):
+    id: str
+    direction: Direction3D
+
+class SnakeRequest(BaseModel):
+    snakes: List[SnakeRequestItem]
+
+class Snake(BaseModel):
+    id: str
+    direction: Direction3D
+    old_direction: Optional[Direction3D]
+    geometry: List[Point3D]
+    death_count: int
     status: str
-    reviveRamainMs: int
+    revive_remain_ms: int
 
-class EnemyShake(BaseModel):
-    geometry: list 
+class Enemy(BaseModel):
+    geometry: List[Point3D]
     status: str
     kills: int
 
 class Food(BaseModel):
-    c: list
+    c: Point3D
     points: int
 
-class BaseInfo(BaseModel):
-    mapSize: int
+class SpecialFood(BaseModel):
+    golden: List[Point3D]
+    suspicious: List[Point3D]
+
+class GameState(BaseModel):
+    map_size: List[int]
     name: str
     points: int
-    fences: list
-    snakes: list[OurSnake]
-    enemies: list[EnemyShake]
-    food: list[Food]
-    specialFood: list[Food] 
+    fences: List[Point3D]
+    snakes: List[Snake]
+    enemies: List[Enemy]
+    food: List[Food]
+    special_food: SpecialFood
     turn: int
-    tickRemainMs: int
-    reviveTimeoutSec: int
-    errors: list
+    revive_timeout_sec: int
+    tick_remain_ms: int
+    errors: Optional[List[str]]
+
+class Round(BaseModel):
+    name: str
+    start_at: datetime
+    end_at: datetime
+    duration: int
+    status: str
+    repeat: int
+
+class GameRoundsResponse(BaseModel):
+    game_name: str
+    rounds: List[Round]
+    now: datetime
