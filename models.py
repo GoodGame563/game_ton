24489c38117a259387514fb32886c6debf8d1829
GoodGame563 <124364912+GoodGame563@users.noreply.github.com>
2024-12-20 18:26:00 +0300

    Update models.py

--- models.py
+++ models.py
@@ -1,3 +1,33 @@
 from pydantic import BaseModel
 
+class OurSnake(BaseModel):
+    id:int
+    direction: str
+    oldDirection: str
+    geometry: list
+    deathCount: int
+    status: str
+    reviveRamainMs: int
 
+class EnemyShake(BaseModel):
+    geometry: list 
+    status: str
+    kills: int
+
+class Food(BaseModel):
+    c: list
+    points: int
+
+class BaseInfo(BaseModel):
+    mapSize: int
+    name: str
+    points: int
+    fences: list
+    snakes: list[OurSnake]
+    enemies: list[EnemyShake]
+    food: list[Food]
+    specialFood: list[Food] 
+    turn: int
+    tickRemainMs: int
+    reviveTimeoutSec: int
+    errors: list
